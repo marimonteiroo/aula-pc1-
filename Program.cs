1464c6a5ba41c89bbe2f2da5f40adca5ca822bb6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 22:09:22 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string opcao = "";
+ 
+while (opcao != "4")
+{
+    Console.WriteLine($"--- Menu ---");
+    Console.WriteLine($"1 - icluir");
+    Console.WriteLine($"2 - alterar");
+    Console.WriteLine($"3 - excluir");
+    Console.WriteLine($"4 - sair");
+ 
+    if (opcao == "1")
+    {
+        Console.WriteLine("um");
+    }
+    else if (opcao == "2")
+    {
+        Console.WriteLine("dois");
+    }
+    else if (opcao == "3")
+    {
+        Console.WriteLine("tres");
+    }
+    opcao = Console.ReadLine()!;
+}
